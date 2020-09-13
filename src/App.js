ceb2ca79dca48108edca79b8f3bd3de781624294
betisairam <sairambeti@gmail.com>
2020-09-13 10:07:48 +0530

    slack search

--- src/App.js
+++ src/App.js
@@ -1,24 +1,125 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import searchData from './Mock/searchData'
 
+import 'react-responsive-modal/styles.css';
+import { Modal } from 'react-responsive-modal';
+const icons = {
+  user: <i className="fa fa-user" aria-hidden="true"></i>,
+  messages: <i className="fa fa-comments-o" aria-hidden="true"></i>,
+  files: <i className="fa fa-files-o" aria-hidden="true"></i>,
+  channel: <i className="fa fa-book" aria-hidden="true"></i>
+}
 function App() {
+  const [search, setSearch] = useState("")
+  const [grp, setGrp] = useState("")
+  const [open, setOpen] = useState("")
+  const [filter, setFitler] = useState([])
+
+  const handleChange = (event) => {
+    setSearch(event.target.value)
+    if (grp) {
+      if (event.target.value.length) {
+        setFitler(searchData?.[grp.name].filter(({ name }) => name.toLowerCase().match(event.target.value.toLowerCase())))
+      }
+      else {
+        setFitler([])
+      }
+    }
+    else {
+      handleNormalSearch(event.target.value)
+    }
+  }
+
+  const handleClick = (name) => {
+    console.log(name);
+    setGrp({
+      name: name,
+      icon: <i className="fa fa-times" aria-hidden="true"></i>
+    })
+  }
+
+  const handleNormalSearch = (value) => {
+    if (value.length) {
+      let initialfilter = []
+      let finalFilter = []
+      for (let index = 0; index < Object.keys(searchData).length; index++) {
+        initialfilter.push(
+          Object.values(searchData)[index].
+            filter(({ name }) => name.toLowerCase().match(value.toLowerCase())))
+        initialfilter[index].map(name => finalFilter.push(name))
+      }
+      setFitler(finalFilter)
+    }
+    else {
+      setFitler([])
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="search_wrap">
+        <div className="search_icon">
+          <i className="fa fa-search" aria-hidden="true"></i>
+        </div>
+        <input onClick={() => setOpen(!open)} placeholder="Search" className="form-control" />
+      </div>
+      <div className="user-modal">
+        <Modal open={open} onClose={() => setOpen(!open)}>
+          <div className="custom-modal">
+            <div className="form-control d-flex" >
+              {grp?.name}
+              {grp?.icon ?
+                <div className="ml-2 mr-3" onClick={() => {
+                  setGrp("")
+                  setFitler([])
+                  handleNormalSearch(search)
+                }
+                }>
+                  {grp?.icon}
+                </div>
+                : null
+              }
+              <input placeholder="Enter" value={search} onChange={handleChange} autoFocus={true} />
+            </div>
+            <div className="filter-list">
+              {
+                filter.map(({ name, icon, user }, index) => {
+                  return (
+                    <div key={index}>
+                      {icons[icon]}
+                      {
+                        user ?
+                          <span className="mr-2">{user}</span> : null
+                      }
+                      {name}
+                    </div>
+                  )
+                })
+              }
+            </div>
+            {
+              grp === "" && filter.length <= 0 ?
+                <div className="icon-group-wrap">
+                  {
+                    Object.keys(icons).map((name, index) => {
+                      return (
+                        <div className="icon-btn" key={index}>
+                          <div className="icon-group" onClick={() => handleClick(name)} >
+                            {
+                              Object.values(icons)[index]
+                            }
+                            <span>{name}</span>
+                          </div>
+                        </div>
+                      )
+                    })
+                  }
+                </div> : null
+            }
+          </div>
+        </Modal>
+      </div>
     </div>
   );
 }
---
+++ src/Mock/searchData.js
@@ -0,0 +1,95 @@
+
+
+export default {
+    user: [
+        {
+            name: "Sairam",
+            icon: "user"
+        },
+        {
+            name: "Vimal",
+            icon: "user"
+        },
+        {
+            name: "Ajay",
+            icon: "user"
+        },
+        {
+            name: "Bipin",
+            icon: "user"
+        },
+        {
+            name: "Shantilal",
+            icon: "user"
+        },
+        {
+            name: "Milan",
+            icon: "user"
+        },
+        {
+            name: "Yogesh",
+            icon: "user"
+        },
+        {
+            name: "Ajay",
+            icon: "user"
+        },
+        {
+            name: "Bhavin",
+            icon: "user"
+        }],
+    channel: [
+        {
+            name: "general",
+            icon: "channel"
+        },
+        {
+            name: "tagline",
+            icon: "channel"
+        },
+        {
+            name: "slack",
+            icon: "channel"
+        },
+        {
+            name: "bot",
+            icon: "channel"
+        },
+    ],
+    files: [
+        {
+            name: "text.txt",
+            icon: "files"
+        },
+        {
+            name: "personal.csv",
+            icon: "files"
+        },
+        {
+            name: "search.csv",
+            icon: "files"
+        }
+    ],
+    messages: [
+        {
+            user: "Sairam",
+            name: "hi how are you",
+            icon: "user"
+        },
+        {
+            user: "Vimal",
+            name: "Fine",
+            icon: "user"
+        },
+        {
+            user: "Ajay",
+            name: "Looking for react developer",
+            icon: "user"
+        },
+        {
+            user: "Client",
+            name: "I have work",
+            icon: "user"
+        },
+    ]
+}
